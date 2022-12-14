5725606f2e6a161b8c20a1f699cb111e37bec6a7
shravanidhanpal <shravanidhanpal20@gmail.com>
2022-12-14 21:46:57 +0530

    getelementbytagname

--- dom.js
+++ dom.js
@@ -18,10 +18,17 @@
 //ti.style.fontWeight= 'bold';
 //ti.style.color='green';
 
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[2]);
-items[2].style.backgroundColor='green';
-for (var i=0; i < items.length; i++){
-    items[i].style.fontWeight='bold';
+//var items=document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[2]);
+//items[2].style.backgroundColor='green';
+//for (var i=0; i < items.length; i++){
+//    items[i].style.fontWeight='bold';
+//}
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[2]);
+li[2].style.backgroundColor='green';
+for (var i=0; i < li.length; i++){
+    li[i].style.fontWeight='bold';
 }
