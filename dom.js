e89d5ae42d998bacf0e1d527f8d0fa5bf488c12c
shravanidhanpal <shravanidhanpal20@gmail.com>
2022-12-14 22:56:27 +0530

    queryselector performed

--- dom.js
+++ dom.js
@@ -1,34 +1,8 @@
-//console.dir(document);
-//console.log(document.domain);
-//console.log(document.doctype);
-//console.log(document.body);
-//console.log(document.all[10]);
-//console.log(document.all);
-//console.log(document.forms);
-
-//var ht=document.getElementById('header-title');
-//console.log(ht);
-//ht.textContent=('Title');
-//ht.innerText=('hello');
-//console.log(ht.innerText);
-//var header=document.getElementById('main-header');
-//header.style.borderBottom='solid 3px black';
-//console.log(document.all);
-//var ti=document.getElementById('title');
-//ti.style.fontWeight= 'bold';
-//ti.style.color='green';
-
-//var items=document.getElementsByClassName('list-group-item');
-//console.log(items);
-//console.log(items[2]);
-//items[2].style.backgroundColor='green';
-//for (var i=0; i < items.length; i++){
-//    items[i].style.fontWeight='bold';
-//}
-var li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[2]);
-li[2].style.backgroundColor='green';
-for (var i=0; i < li.length; i++){
-    li[i].style.fontWeight='bold';
-}
+var header= document.querySelector('#main-header');
+header.style.borderBottom='solid 4px black';
+var input=document.querySelector('input');
+input.value='hello world';
+var  secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+var thirditem=document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.display='none';
